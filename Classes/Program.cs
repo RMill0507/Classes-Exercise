ad5110cadcddb3cc35d237eb491ba82bd55e3fe2
Rob <Rmilla445@gmail.com>
2022-10-27 13:51:25 -0400

    myCar class

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();//Contructor -Creates new instance in Car class
+            myCar.Make = "Chevy";
+            myCar.Model = "Sliverado";
+            myCar.Year = "2014";
+            Console.WriteLine(myCar.Make); 
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
+
+            var ford = new Car()
+            {
+                Make = "Ford",
+                Model = "Raptor",
+                Year = "2018"
+            };
+
         }
     }
 }
